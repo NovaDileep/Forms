e618178ea2a40220ae5779f87d7f0ff6eb6e5831
NovaDileep <nova.cs17@jecc.ac.in>
2021-06-14 18:12:24 +0530

    forms using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Forms';
+  User={username:'',
+        password:''};
+  userverify() {
+    alert("form submitted");
+  }      
 }
